a83c1835a593878461aa765bc3675b07e716d5de
Weverton Silva <weverton1995@hotmail.com>
2019-05-25 00:01:58 -0300

    gabarito questao 1a edl 2019.1

---
+++ teste 2 - 2019.1/folhas.hs
@@ -0,0 +1,11 @@
+data Arvore = Folha | Galho Int [Arvore]
+                deriving Show
+
+a1 = Galho 10 [Galho 20 [Folha, Folha], Galho 30 [Folha, Folha]]
+a2 = Galho 20 [Galho 30 [Folha, Folha], Folha]
+
+folhas :: Arvore -> Int
+folhas Folha = 1
+folhas (Galho n (h : t)) = (folhas h) + foldr(\x acc -> acc + (folhas x)) 0 t
+
+main = print(folhas a1)
