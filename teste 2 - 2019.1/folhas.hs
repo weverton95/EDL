5b28ec1cf1424554fb045658aeb523ac26c542b4
Weverton Silva <weverton1995@hotmail.com>
2019-05-25 03:50:31 -0300

    Indentação do código

--- teste 2 - 2019.1/folhas.hs
+++ teste 2 - 2019.1/folhas.hs
@@ -5,7 +5,7 @@ a1 = Galho 10 [Galho 20 [Folha, Folha], Galho 30 [Folha, Folha]]
 a2 = Galho 20 [Galho 30 [Folha, Folha], Folha]
 
 folhas :: Arvore -> Int
-folhas Folha = 1
-folhas (Galho n (h : t)) = (folhas h) + foldr(\x acc -> acc + (folhas x)) 0 t
+folhas Folha                = 1
+folhas (Galho n (h : t))    = (folhas h) + foldr(\x acc -> acc + (folhas x)) 0 t
 
 main = print(folhas a1)
