387f4033a59712468022673c2139cd15f90664f3
Weverton Leite Da Silva <weverton1995@hotmail.com>
2019-05-29 20:22:06 -0300

    melhoria no código

--- teste 2 - 2019.1/map.hs
+++ teste 2 - 2019.1/map.hs
@@ -4,8 +4,8 @@ data Arvore = Folha | Galho Int [Arvore]
 a1 = Galho 10 [Galho 20 [Folha, Folha, Galho 30 [Folha]], Galho 40 [Folha, Folha]]
 a2 = Galho 20 [Galho 30 [Folha, Folha], Folha]
 
-mapA :: (Int -> Int) -> Arvore -> Arvore 
-mapA f Folha                = Folha
-mapA f (Galho n (h : t))    = (Galho (f n) ([] ++ [mapA f h] ++ (map(\x -> mapA f x) t)))
+mapA :: (Int -> Int) -> Arvore -> Arvore
+mapA f Folha = Folha
+mapA f (Galho n a) = Galho (f n) (map (\x -> mapA f x) a)
 
 main = print(mapA (\x -> 2 * x) a1)
